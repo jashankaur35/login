edbb8206445fe74eb987dd5bad0fb02dc8876d2d
jashanpreet kaur035 <113895705+jashankaur35@users.noreply.github.com>
2025-02-22 19:17:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <title>Registration form</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style1.css">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
 <body>
